76e9ecabb1434ac099281eff7a55bd24f2dfd922
Bohdan Volkov <bohdan.volkov@dxc.com>
2022-05-16 15:50:45 +0300

    add env variable

--- src/redux/actions/products.js
+++ src/redux/actions/products.js
@@ -3,7 +3,7 @@ import types from "../types";
 export const getProducts = () => async (dispatch) => {
     dispatch({type: types.products.GET_PRODUCTS_DATA_REQUEST})
     try {
-      const res = await fetch('http://localhost:3000/api/products')
+      const res = await fetch(`${process.env.API_HOST}/products`)
       const data = await res.json()
       dispatch({type: types.products.GET_PRODUCTS_DATA_SUCCESS, payload: data})
     } catch (e) {
