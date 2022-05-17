c7f1416f4acc5aece00c78cd9f48e5fbab26588e
Bohdan Volkov <bohdan.volkov@dxc.com>
2022-05-17 12:04:26 +0300

    add ssr progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
-import '../src/styles/index.scss'
+import NextNProgress from "nextjs-progressbar";
 import {wrapper} from "../src/redux/store";
+import '../src/styles/index.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return (
+    <>
+      <NextNProgress
+        color="#CFF69B"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
+    </>
   )
 }
 
